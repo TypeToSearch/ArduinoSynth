d0d3e6a676527026971ec470eab666b987fc26c3
Ben Thomas <thomben4@isu.edu>
2024-03-08 11:58:29 -0700

    Experimenting with slider options

--- Waveform Drawing App/main.py
+++ Waveform Drawing App/main.py
@@ -1,5 +1,11 @@
 import kivy
 from kivy.app import App
+from kivy.app import Widget
+
+
+class SampleSlider(Widget):
+    def on_touch_move(self, touch):
+        print("Mouse moved to", touch.pos)
 
 
 class SamplerApp(App):
