bc50e5af99215019f9e116800f67a0da5faea231
Ben Thomas <thomben4@isu.edu>
2024-03-10 11:29:14 -0600

    Multiple rectangles not working

--- Waveform Drawing App/main.py
+++ Waveform Drawing App/main.py
@@ -5,16 +5,27 @@ from kivy.graphics import *
 
 
 class SampleSlider(Widget):
-    def __init__(self, **kwargs):
+    def __init__(self, num_sliders=1, default=0.5, minimum=0, maximum=1, **kwargs):
         super(SampleSlider, self).__init__(**kwargs)
+        self.tracks = []
+        # Create slider tracks
         with self.canvas:
-            Color(0, 0, 1)
-            self.rect = Rectangle(pos=self.pos, size=self.size)
+            Color(0.83, 0.83, 0.83)
+            for i in range(num_sliders):
+                size = (self.size[0] / num_sliders, self.size[1])
+                position = (self.pos[0] + (size[0] * i), self.pos[1])
+                self.tracks.append(Rectangle(pos=position, size=size))
+                Color(0, 0, 1)
+
         self.bind(pos=self.update_rect, size=self.update_rect)
 
     def update_rect(self, *args):
-        self.rect.pos = self.pos
-        self.rect.size = self.size
+        sliders = len(self.tracks)
+        for i in range(sliders):
+            size = (self.size[0] / sliders, self.size[1])
+            position = (self.pos[0] + (size[0] * i), self.pos[1])
+            self.tracks[i].size = size
+            self.tracks[i].pos = position
 
     def on_touch_move(self, touch):
         print("Mouse moved to", touch.pos)
