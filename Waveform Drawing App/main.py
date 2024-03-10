f3d4a81842c56c1e1531f4f968faeedb782449c2
Ben Thomas <thomben4@isu.edu>
2024-03-10 14:05:16 -0600

    Working on adding slides to sliders

--- Waveform Drawing App/main.py
+++ Waveform Drawing App/main.py
@@ -11,6 +11,7 @@ class SampleSlider(Widget):
     def __init__(self, **kwargs):
         super(SampleSlider, self).__init__(**kwargs)
         self.tracks = []
+        self.slides = []
 
         self.bind(num_sliders=self.initialize_tracks)
         self.bind(pos=self.update_rect, size=self.update_rect)
@@ -24,16 +25,33 @@ class SampleSlider(Widget):
                 position = (self.pos[0] + (size[0] * i), self.pos[1])
                 self.tracks.append(Rectangle(pos=position, size=size))
 
+        with self.canvas:
+            Color(0, 0, 1)
+            for i in range(self.num_sliders):
+                size = (self.size[0] / self.num_sliders, self.size[1] / 10)
+                position = (self.pos[0] + (size[0] * i), self.pos[1])
+                self.slides.append(Rectangle(pos=position, size=size))
+
     def update_rect(self, *args):
-        sliders = len(self.tracks)
-        for i in range(sliders):
-            size = (self.size[0] / sliders, self.size[1])
+        num = len(self.tracks)
+        for i in range(num):
+            size = (self.size[0] / num, self.size[1])
             position = (self.pos[0] + (size[0] * i), self.pos[1])
             self.tracks[i].size = size
             self.tracks[i].pos = position
 
+        num = len(self.slides)
+        for i in range(num):
+            size = (self.size[0] / self.num_sliders, self.size[1] / 10)
+            position = (self.pos[0] + (size[0] * i), self.pos[1])
+            self.slides[i].size = size
+            self.slides[i].pos = position
+
     def on_touch_move(self, touch):
         print("Mouse moved to", touch.pos)
+
+    def on_touch_down(self, touch):
+        print("Mouse click at", touch.pos)
 
 
 class SamplerApp(App):
