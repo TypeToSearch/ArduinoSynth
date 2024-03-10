b19b834b6effd8e624b8f4796b01b6b20a841d92
Ben Thomas <thomben4@isu.edu>
2024-03-10 13:50:36 -0600

    Working custom property with kv file

--- Waveform Drawing App/main.py
+++ Waveform Drawing App/main.py
@@ -2,22 +2,27 @@ import kivy
 from kivy.app import App
 from kivy.app import Widget
 from kivy.graphics import *
+from kivy.properties import NumericProperty
 
 
 class SampleSlider(Widget):
-    def __init__(self, num_sliders=1, default=0.5, minimum=0, maximum=1, **kwargs):
+    num_sliders = NumericProperty(1)
+
+    def __init__(self, **kwargs):
         super(SampleSlider, self).__init__(**kwargs)
         self.tracks = []
-        # Create slider tracks
+
+        self.bind(num_sliders=self.initialize_tracks)
+        self.bind(pos=self.update_rect, size=self.update_rect)
+
+    def initialize_tracks(self, *args):
+        self.tracks = []
         with self.canvas:
             Color(0.83, 0.83, 0.83)
-            for i in range(num_sliders):
-                size = (self.size[0] / num_sliders, self.size[1])
+            for i in range(self.num_sliders):
+                size = (self.size[0] / self.num_sliders, self.size[1])
                 position = (self.pos[0] + (size[0] * i), self.pos[1])
                 self.tracks.append(Rectangle(pos=position, size=size))
-                Color(0, 0, 1)
-
-        self.bind(pos=self.update_rect, size=self.update_rect)
 
     def update_rect(self, *args):
         sliders = len(self.tracks)
